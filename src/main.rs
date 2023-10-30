1fbeb2b83e0c308a63a3557e2894365d18b13021
Maxim <petsol.reserve228@gmail.com>
2023-10-30 04:13:49 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, update_test)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((Name("Player".to_string()), Position { x: 0., y: 0. }));
+    commands.spawn((Name("Enemy".to_string()), Position { x: 100., y: 0. }));
+}
+
+fn update_test(
+    time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut person_position: Query<(&mut Position, &Name)>,
+) {
+    for (mut position, name) in &mut person_position {
+        if keyboard_input.pressed(KeyCode::D) {
+            position.x -= 150. * time.delta_seconds();
+        }
+
+        if keyboard_input.pressed(KeyCode::A) {
+            position.x += 150. * time.delta_seconds();
+        }
+
+        println!("{0:?} position: {1}", name.0, position.x);
+    }
+}
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component, Debug)]
+struct Name(String);
